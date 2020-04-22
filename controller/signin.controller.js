1f95a5e34850ad45c0fa95eac26eff5795bdb246
amishapatel188 <amishapatel.188@gmail.com>
2020-04-21 22:55:15 -0700

    signin controller bug fix

--- controller/signin.controller.js
+++ controller/signin.controller.js
@@ -1,5 +1,6 @@
 exports.checkValidation = (req, res, next) => {
     // token is at req.body.data => validate it
     token = req.body.data;
+    console.log("Signin request")
     res.status(200).json({'msg': 'Valid login', 'status': true});
 }
