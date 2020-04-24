a387d35b2a556ac945d1f9083633d8027845f558
amishapatel188 <amishapatel.188@gmail.com>
2020-04-23 18:39:17 -0700

    added restaurants details based on lat and longitude

--- controller/restaurants.controller.js
+++ controller/restaurants.controller.js
@@ -1,11 +1,12 @@
 const request = require('request');
 const authHeader = 'Bearer WdMlzizMaL5u0D3bCUfPvfsk-4v8VmjMzZhhn-2Vq0P5j1gwtx9VDWtTidi18LqkT2uJ2gbbsw63yByVjcp19Wv_jLcAkrvW2fukofv-wgkBLhMNmRX01xAQimJiXHYx';
-const getRestaurantApi = 'https://api.yelp.com/v3/businesses/search?location=nutwood ave fullerton';
 const getReviewsApi = 'https://api.yelp.com/v3/businesses/E950qgee4B4qbvkOmEPWEA/reviews';
 
 exports.getRestaurantsList = (reqM, resM, nextM) => {
     // call yelp api
     //request.header.authorization = 'Bearer WdMlzizMaL5u0D3bCUfPvfsk-4v8VmjMzZhhn-2Vq0P5j1gwtx9VDWtTidi18LqkT2uJ2gbbsw63yByVjcp19Wv_jLcAkrvW2fukofv-wgkBLhMNmRX01xAQimJiXHYx';
+    getRestaurantApi = "https://api.yelp.com/v3/businesses/search?term=" + reqM.query.searchValue + "&latitude=" +reqM.query.lat + "&longitude="+ reqM.query.longi;
+    console.log(getReviewsApi);
     request(getRestaurantApi, {json:true}, (err, res, body) => {
         resM.send({"msg":body}); 
     }).setHeader('authorization', authHeader )   
