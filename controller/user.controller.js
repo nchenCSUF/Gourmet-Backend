d6642154ec51a4b65cbae214894f32ad4ab272ea
Amisha <amishapatel.188@gmail.com>
2020-04-28 12:06:22 -0700

    mongodb connection string changes

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,24 +1,28 @@
 const user = require('../model/user.model');
-
+const mongoose = require('mongoose');
 exports.createUser = (req, res, next) => {
-    console.log(req)
+    console.log(mongoose)
     const {id,email,firstName, lastName, type, preference, fav} = req.body;
     const item = new user({
         id,email,firstName, lastName, type, preference, fav
     });
-    msg="saved"
-    item.save(function (err) {
+    console.log("Item: " , item);
+    msg="saved";
+
+    item.save(function (err,data) {
         if (err) {
             msg = "error" + err
         }
+        console.log("res", res);
+        res.status(200).json({'msg': msg});
       });
-    res.status(200).json({'msg': msg});
+    
 }
 
 exports.getUser = (req, res, next) => {
     id = req.query.id;
     console.log("sign in request" , req)
-    user.find({"email":id},(err, data) => {
+    user.find({"email":id},(err, data) => { 
         if(err) {
            console.error(err)
         }
--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const routes = require('./routes');
 // const connect = require('./model/connect')
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://gourmetdb:LhIU7IBZKsdJKz1cjP4iITDQW3FYKgGxOQq4pbo9ZZi99eVms3tiMLm9Gmd8hP5sBz3Lx2DCZ3qhHptZgaNSCw==@gourmetdb.mongo.cosmos.azure.com:10255/gourmetdb?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@gourmetdb@',{ useUnifiedTopology: true, useNewUrlParser: true }
+mongoose.connect('mongodb://gourmetdb:LhIU7IBZKsdJKz1cjP4iITDQW3FYKgGxOQq4pbo9ZZi99eVms3tiMLm9Gmd8hP5sBz3Lx2DCZ3qhHptZgaNSCw==@gourmetdb.mongo.cosmos.azure.com:10255/gourmetdb?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@gourmetdb@',{ useUnifiedTopology: true, useNewUrlParser: true }
 )
 //mongodb+srv://gourmet:gourmet@cluster0-vr9mv.mongodb.net/gourmet-db?retryWrites=true&w=majority'
- //mongodb://gourmetdb:LhIU7IBZKsdJKz1cjP4iITDQW3FYKgGxOQq4pbo9ZZi99eVms3tiMLm9Gmd8hP5sBz3Lx2DCZ3qhHptZgaNSCw==@gourmetdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@gourmetdb@
+ //mongodb://gourmetdb:LhIU7IBZKsdJKz1cjP4iITDQW3FYKgGxOQq4pbo9ZZi99eVms3tiMLm9Gmd8hP5sBz3Lx2DCZ3qhHptZgaNSCw==@gourmetdb.mongo.cosmos.azure.com:10255/gourmetdb?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@gourmetdb@
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function() {
+db.on('open', function() {
     console.log('we are connected')
+    mongoose.connection.db.listCollections().toArray(function (err, names) {
+        console.log(names); // [{ name: 'dbname.myCollection' }]
+    });
 });
-   
+
+
 
 const app = express();
 app.use(bodyParser.json({limit:'50mb'}));
