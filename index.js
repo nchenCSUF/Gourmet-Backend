4c73f4d7122a99a9897e1fe24b15819b9537e828
Amisha <amishapatel.188@gmail.com>
2020-04-28 16:45:29 -0700

    connection issue fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const routes = require('./routes');
 // const connect = require('./model/connect')
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://gourmet:gourmet@cluster0-vr9mv.mongodb.net/gourmet-db?retryWrites=true&w=majority',{ useUnifiedTopology: true, useNewUrlParser: true }
+mongoose.connect('mongodb://gourmetdb:LhIU7IBZKsdJKz1cjP4iITDQW3FYKgGxOQq4pbo9ZZi99eVms3tiMLm9Gmd8hP5sBz3Lx2DCZ3qhHptZgaNSCw==@gourmetdb.mongo.cosmos.azure.com:10255/gourmetdb?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@gourmetdb@',{ useUnifiedTopology: true, useNewUrlParser: true }
 )
 //mongodb+srv://gourmet:gourmet@cluster0-vr9mv.mongodb.net/gourmet-db?retryWrites=true&w=majority'
  //mongodb://gourmetdb:LhIU7IBZKsdJKz1cjP4iITDQW3FYKgGxOQq4pbo9ZZi99eVms3tiMLm9Gmd8hP5sBz3Lx2DCZ3qhHptZgaNSCw==@gourmetdb.mongo.cosmos.azure.com:10255/gourmetdb?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@gourmetdb@
